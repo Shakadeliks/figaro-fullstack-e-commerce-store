30816e0d434d4e86101490a79515f2f8c3968bd7
Shakadeliks <shakurxmatiwana@gmail.com>
2022-03-10 21:21:15 +0200

    began front-end

---
+++ figaro-front-end/src/App.js
@@ -0,0 +1,12 @@
+import { CssBaseline } from '@mui/material/CssBaseline';
+
+function App() {
+  return (
+    <div className="App">
+       <CssBaseline />
+
+    </div>
+  );
+}
+
+export default App;
