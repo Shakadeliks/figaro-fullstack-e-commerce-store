1bef663770d19d06cda2fcaf3ca14093ae1a542f
Shakadeliks <shakurxmatiwana@gmail.com>
2022-03-16 22:11:25 +0200

    carousel implemented

--- figaro-front-end/src/components/Carousel/Carousel.styles.js
+++ figaro-front-end/src/components/Carousel/Carousel.styles.js
@@ -4,6 +4,8 @@ export const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
+    position: relative;
+    overflow: hidden;
 `
 
 export const Arrow = styled.div`
@@ -15,5 +17,62 @@ export const Arrow = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: ${ props => props.direction === "left" && "10px"};
+    right: ${ props => props.direction === "right" && "10px"};
+    margin: auto;
+    cursor: pointer;
+    opacity: 0.7;
+    
+`
+export const CarouselWrapper = styled.div`
+    height: 100%;
 `
 
+export const SlideContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+`
+
+export const ImgContainer = styled.div`
+    height: 100%;
+    flex: 1;
+`
+
+export const Img = styled.img`
+    height: 80%;
+`
+
+export const InfoContainer = styled.div`
+    flex: 1;
+    padding: 3.125rem;
+`
+
+export const SlideTitle = styled.h2`
+    font-size: 4.375rem;
+    text-transform: uppercase;
+    margin-bottom: 0;
+    margin-top: 0;
+`
+
+export const SlideDesc = styled.p`
+    margin: 2.5rem 0 2.5rem 0;
+    font-size: 1.25rem;
+    font-weight: 500;
+    letter-spacing: 3px;
+`
+export const SlideBtn = styled.button`
+    padding: 0.625rem;
+    font-size: 1.25rem;
+    cursor: pointer;
+    background-color: ${ ({ theme: {secondary} }) => secondary.main};
+    border-style: none;
+    border-radius: 0.5rem;
+    text-transform: uppercase;
+    font-weight: 400;
+`
--- figaro-front-end/src/components/Carousel/index.js
+++ figaro-front-end/src/components/Carousel/index.js
@@ -1,14 +1,43 @@
-import { Container, Arrow } from './Carousel.styles';
+import { 
+    Container,
+    Arrow, 
+    CarouselWrapper, 
+    SlideContainer, 
+    Img,
+    ImgContainer, 
+    InfoContainer,
+    SlideTitle,
+    SlideDesc,
+    SlideBtn} from './Carousel.styles';
 import React from 'react';
 import { ArrowBackRounded, ArrowForwardRounded } from '@material-ui/icons';
+import CarouselImg from '../../assets/carousel-img1.jpg'
 
 const Carousel = () => {
   return (
     <Container>
-        <Arrow>
+        <Arrow direction="left">
             <ArrowBackRounded />
         </Arrow>
-        <Arrow>
+        <CarouselWrapper>
+          <SlideContainer>
+            <ImgContainer>
+              <Img src={CarouselImg}/>
+            </ImgContainer>         
+            <InfoContainer>
+              <SlideTitle>
+                fresh winter jackets
+              </SlideTitle>
+              <SlideDesc>
+                don't miss out on our new range of designer jackets
+              </SlideDesc>
+              <SlideBtn>
+                shop now
+              </SlideBtn>
+            </InfoContainer>
+          </SlideContainer>  
+        </CarouselWrapper>
+        <Arrow direction="right">
             <ArrowForwardRounded />
         </Arrow>
     </Container>
