294b65e8a18ddc5f9c8eaf5daf1c396151515f8b
Shakadeliks <shakurxmatiwana@gmail.com>
2022-03-17 17:06:03 +0200

    carousel complete

--- figaro-front-end/src/components/Carousel/Carousel.styles.js
+++ figaro-front-end/src/components/Carousel/Carousel.styles.js
@@ -26,10 +26,14 @@ export const Arrow = styled.div`
     margin: auto;
     cursor: pointer;
     opacity: 0.7;
-    
+    z-index: 2;
 `
 export const CarouselWrapper = styled.div`
     height: 100%;
+    display: flex;
+    transform: translateX(${props => props.carouselIndex * -100}vw);
+
+    transition: all 1.5s ease
 `
 
 export const SlideContainer = styled.div`
@@ -37,6 +41,7 @@ export const SlideContainer = styled.div`
     height: 100vh;
     display: flex;
     align-items: center;
+    background-color:  ${ ({ theme: {primary} }) => primary.light};
 `
 
 export const ImgContainer = styled.div`
@@ -51,6 +56,7 @@ export const Img = styled.img`
 export const InfoContainer = styled.div`
     flex: 1;
     padding: 3.125rem;
+    color:  ${ ({ theme: {textCol} }) => textCol.textOnPrim};
 `
 
 export const SlideTitle = styled.h2`
@@ -58,6 +64,7 @@ export const SlideTitle = styled.h2`
     text-transform: uppercase;
     margin-bottom: 0;
     margin-top: 0;
+    
 `
 
 export const SlideDesc = styled.p`
@@ -65,14 +72,17 @@ export const SlideDesc = styled.p`
     font-size: 1.25rem;
     font-weight: 500;
     letter-spacing: 3px;
+    text-transform: uppercase;
+    
 `
 export const SlideBtn = styled.button`
     padding: 0.625rem;
     font-size: 1.25rem;
     cursor: pointer;
-    background-color: ${ ({ theme: {secondary} }) => secondary.main};
-    border-style: none;
-    border-radius: 0.5rem;
+    background-color: transparent;
+    border: 2px solid inherit;
+    border-radius: 0.2rem;
     text-transform: uppercase;
     font-weight: 400;
+    color: inherit;
 `
---
+++ figaro-front-end/src/data.js
@@ -0,0 +1,59 @@
+import CarouselImg1 from './assets/carousel-img1.jpg';
+import CarouselImg2 from './assets/carousel-img2.jpg';
+import CarouselImg3 from './assets/carousel-img3.jpg';
+import CarouselImg4 from './assets/carousel-img4.jpg';
+
+
+export const carouselItems = [
+    {
+        id: 1,
+        img: CarouselImg1,
+        title: "stylish winter jackets",
+        desc: "don't miss out on our new range of designer jackets",
+        
+    },
+    {
+        id: 2,
+        img: CarouselImg2,
+        title: "denim deals",
+        desc: "save up to 25% on our latest designer jeans",
+        
+    },
+    {
+        id: 3,
+        img: CarouselImg3,
+        title: "hottest sneakers",
+        desc: "shop the most in-demand men's sneakers",
+        
+    },
+    {
+        id: 4,
+        img: CarouselImg4,
+        title: "curated designer brands",
+        desc: "we have the finest selection of men's designers to keep you in style",
+        
+    },
+]
+
+export const productCategories = [
+    {
+        id: 1,
+        title: "hats",
+        img: ""
+    },
+    {
+        id: 2,
+        title: "shirts",
+        img: ""
+    },
+    {
+        id: 1,
+        title: "pants",
+        img: ""
+    },
+    {
+        id: 1,
+        title: "sneakers",
+        img: ""
+    },
+]
